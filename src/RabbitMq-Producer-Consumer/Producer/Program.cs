308f5c03a4b98e3849ae4649c2960c056e359e26
Crispin Tuballa <cristuballa@gmail.com>
2023-02-17 15:39:16 +1300

    Add RabbitMQ producer cosumer direct exchange

---
+++ src/RabbitMq-Producer-Consumer/Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace Producer;
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        var count = 0;
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672")
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare("demo-queue",
+                             durable: true,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null
+                             );
+        while(true)
+        {
+            var message= new { Name = $"Producer {count}", Message = $"Hello from producer {count}" };
+            var body=Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+            channel.BasicPublish("","demo-queue",basicProperties: null,body: body);
+            Console.WriteLine($"Message {count} sent");
+            Task.Delay(1).Wait();
+            count++;
+        }
+
+    }
+}
